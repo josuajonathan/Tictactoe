b5fbf60b1d25c2a278c55104280ef9055766fae0
Josua Jonathan Manurung <87791029+josuajonathan@users.noreply.github.com>
2022-03-31 00:58:58 +0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,11 +1,11 @@
 #pragma once
+
 class Board
 {
-public :
-	Board();
+public:
 	void clearBoard();
 	void print();
-	
+
 	void setvalueAtIndex(int x, int y, char value);
 	char getvalueAtIndex(int x, int y);
 
